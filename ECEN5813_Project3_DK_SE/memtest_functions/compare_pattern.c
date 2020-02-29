76a0601b875db2a02ea9bf22eb50b0031700e297
Dhruva Koley <dhruvakoley@gmail.com>
2020-02-28 19:00:57 -0700

    fixed compare

--- ECEN5813_Project3_DK_SE/memtest_functions/compare_pattern.c
+++ ECEN5813_Project3_DK_SE/memtest_functions/compare_pattern.c
@@ -17,22 +17,22 @@
 #include "source/logger.h"
 #include "memtest.h"
 
+bool ret = false;
+
 bool compare_pattern(uint8_t* mem, uint8_t num_bytes, uint8_t seed){
 	uint8_t* new;
 	new = (uint8_t*)malloc(num_bytes*sizeof(uint8_t));
 	new= gen_pattern(num_bytes,seed);
 	for(int i=0;i<num_bytes;i++)
 	{
-	    while(mem[i]!=new[i])
+	    while(mem[i] != new[i])
 	    {
 	    	printf("False");
+	    	ret = false;
+	    	return ret;
 	    }
 	}
-
-
-
-
-	bool ret = true;
+	ret = true;
 	printf("True");
 	//
 	return ret;
