9e241f62fbad3eeab1115582a3f60029dc061edf
Dhruva Koley <dhruvakoley@gmail.com>
2020-03-02 20:52:29 -0700

    fixed invert function

--- ECEN5813_Project3_DK_SE/memtest_functions/display_memory.c
+++ ECEN5813_Project3_DK_SE/memtest_functions/display_memory.c
@@ -17,6 +17,7 @@
 
 mem_status_t dis_m;
 
+// inspired by https://stackoverflow.com/questions/111928/is-there-a-printf-converter-to-print-in-binary-format
 #define BYTE_TO_BINARY_PATTERN " 0b%c%c%c%c%c%c%c%c"
 #define BYTE_TO_BINARY(byte) \
 	(byte & 0x80 ? '1' : '0'), \
@@ -30,23 +31,19 @@ mem_status_t dis_m;
 
 void display_memory(uint8_t* mem, uint8_t num_bytes){
 	if(num_bytes <= MAX_SIZE){
-		uint32_t *ptr = (uint32_t*)mem;
-		int8_t index = 0;
-		int8_t b_index = 0;
+		uint32_t *bin_ptr = (uint32_t*)mem;
 		uint8_t iter = num_bytes / 4;
-		for(index = 0; index < iter; index++){
-			PRINTF("0x%08X 0x%08X", ptr, *ptr);
-			for(b_index = 3; b_index >= 0; b_index--){
-				PRINTF(BYTE_TO_BINARY_PATTERN"\t", BYTE_TO_BINARY(*(((uint8_t *)ptr) + b_index)));
+		for(int8_t index = 0; index < iter; index++){
+			PRINTF("0x%08X 0x%08X", bin_ptr, *bin_ptr);
+			for(int8_t b_index = 3; b_index >= 0; b_index--){
+				PRINTF(BYTE_TO_BINARY_PATTERN"\t", BYTE_TO_BINARY(*(((uint8_t *)bin_ptr) + b_index)));
 			}
 			PRINTF("\r\n");
-			ptr = ((uint32_t*)ptr)+1;
+			bin_ptr = ((uint32_t*)bin_ptr) + 1;
 		}
 		dis_m = MEM_SUCCESS;
-		Log_data(mem, 32);
+//		Log_data(mem, num_bytes);
 	} else{
-//		printf("Error");
-//		printf("\n\r");
 		Log_string("Out of range");
 		dis_m = MEM_FAILED;
 	}
--- ECEN5813_Project3_DK_SE/memtest_functions/invert_block.c
+++ ECEN5813_Project3_DK_SE/memtest_functions/invert_block.c
@@ -17,47 +17,22 @@
 
 mem_status_t inv_b;
 
-// inspired by https://www.geeksforgeeks.org/count-total-bits-number/
-int32_t count_bits(uint32_t n) {
-	uint32_t counter = 0;
-	while(n){
-		counter++;
-		n >>= 1;
-	}
-	return counter;
-}
-
 // inspired by https://www.geeksforgeeks.org/invert-actual-bits-number/
 void invert_block(uint8_t* mem, uint8_t offset, uint8_t num_bytes){
+	// check if out of range
 	if(offset <= MAX_SIZE){
-		uint8_t num_bits;
-		uint8_t* inv = (uint8_t*)calloc(num_bytes, sizeof(uint8_t));
+		// set an intermediate pointer to access the mem alloc
+		uint8_t * inv = (uint8_t*)(mem + offset);
 		if(inv == NULL){
 			inv_b = MEM_FAILED;
-			Log_string("Write memory: split array failed to allocate");
+			Log_string("Write memory: inv array failed to allocate");
 		}else{
-			uint8_t offset1 = offset; // save offset to local var
-			// store values (numbytes = num of indexes) to intermediate array
+			// xor bytes to invert and feed it back in
 			for(int i = 0; i < num_bytes; i++){
-				inv[i] = mem[offset1];
-				offset1++;
+				inv[i] = inv[i] ^ 0xFF;
 			}
-			// invert bits over number of number of bytes (i.e. each index in mem array)
-			for(int j = 0; j < num_bytes; j++){
-				num_bits = count_bits((uint8_t)inv[j]);
-				for(int k = 0; k < num_bits; k++){
-					inv[j] = (inv[j] ^ (1 << k));
-				}
-			}
-			// feed inverted values back to mem array
-			for(int l = 0; l < num_bytes; l++){
-				mem[offset] = inv[l];
-				offset++;
-			}
-			// free intermediate array memory
-			free(inv);
 			inv_b = MEM_SUCCESS;
-			Log_data(mem, 32);
+//			Log_data(mem, 32);
 		}
 	} else{
 		inv_b = MEM_FAILED;
